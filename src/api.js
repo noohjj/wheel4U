4fe9e06c99aaf612aa0ae671ebc2a7242b98c283
priestone <aion97@naver.com>
2024-12-12 14:38:19 +0900

    wheel

--- src/api.js
+++ src/api.js
@@ -1,34 +1,29 @@
-import axios from "axios";
-import xml2js from "xml2js";
+const fetch = require("node-fetch");
 
-// 기본 URL과 서비스 키
 const baseUrl =
-  "http://apis.data.go.kr/B554287/DisabledPersonConvenientFacility";
+  "http://apis.data.go.kr/6260000/BusanFcltsDsgstInfoService/getFcltsDsgstInfo";
 
-const ServiceKey =
+const servicekey =
   "1l64t6Bdj%2FETCziwk1dAKNoPCXX%2BU77BE9UDSsFYQmEfO7%2Be79daO3HTK5T6dFPGIB9PNzP7%2BJlqq1j3YbcBiA%3D%3D";
 
-// API 요청 함수
-export const fetchFacilityData = async (params = {}) => {
-  try {
-    // 기본 요청 파라미터 설정
-    const defaultParams = {
-      ServiceKey: ServiceKey,
-      type: "xml", // XML 형식으로 데이터 요청
-      ...params,
-    };
+const allUrl = `${baseUrl}?serviceKey=${servicekey}&pageNo=1&numOfRows=994&resultType=json`;
 
-    // API 호출
-    const response = await axios.get(baseUrl, { params: defaultParams });
+const option = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+  },
+};
 
-    // XML 데이터를 JSON으로 변환
-    const jsonResult = await xml2js.parseStringPromise(response.data, {
-      explicitArray: false, // 배열을 자동으로 처리하지 않음
+export const WheelData = () => {
+  return fetch(allUrl, option)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log("API 데이터:", data); // API 응답 데이터 출력
+      return data; // 응답 데이터 반환
+    })
+    .catch((error) => {
+      console.log("API 호출 중 오류 발생:", error); // 오류가 발생했을 경우 출력
+      return { results: [] }; // 오류 발생 시 빈 배열 반환
     });
-
-    return jsonResult;
-  } catch (error) {
-    console.error("Error fetching facility data:", error);
-    throw error;
-  }
 };

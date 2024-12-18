74d5b48978f11bb6669d47c367f1fb5052ad97e5
noohjj <yk3216@naver.com>
2024-12-18 12:22:24 +0900

    wheel

--- src/Router.js
+++ src/Router.js
@@ -2,7 +2,6 @@ import { HashRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/home/Home";
 import Location from "./pages/location/Location";
 import Search from "./pages/search/Search";
-import Detail from "./pages/detail/Detail";
 import BookMark from "./pages/bookmark/BookMark";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
@@ -15,7 +14,6 @@ const Router = () => {
         <Route path="/" element={<Home />} />
         <Route path="/location/:locateNm" element={<Location />} />
         <Route path="/search" element={<Search />} />
-        <Route path="/detail/:subject" element={<Detail />} />
         <Route path="/bookmark" element={<BookMark />} />
       </Routes>
       <Footer />

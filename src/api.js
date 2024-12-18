9e31250ad8e7f2e9626f91c3248334c4b50e270a
priestone <aion97@naver.com>
2024-12-18 16:30:07 +0900

    wheel4ufinal

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 const fetch = require("node-fetch");
 
 const baseUrl =
-  "http://apis.data.go.kr/6260000/BusanFcltsDsgstInfoService/getFcltsDsgstInfo";
+  "https://apis.data.go.kr/6260000/BusanFcltsDsgstInfoService/getFcltsDsgstInfo";
 
 const servicekey =
   "1l64t6Bdj%2FETCziwk1dAKNoPCXX%2BU77BE9UDSsFYQmEfO7%2Be79daO3HTK5T6dFPGIB9PNzP7%2BJlqq1j3YbcBiA%3D%3D";
